be653de32876e6b3a6b131572c3590ca78f4b170
jakehume <jakehume@umich.edu>
2023-05-26 16:39:10 -0400

    requirements.txt generated via pipreqs

--- utils.py
+++ utils.py
@@ -4,11 +4,19 @@ import os
 import ast
 import time
 
-def get_initial_message(vocab_word, aux_words):
+
+
+def get_initial_message(vocab_word, aux_words, system_prompt=None, user_prompt=None):
+    if system_prompt is None:
+        print("system_prompt is None")
+        system_prompt = f"""You are a Chinese language professor tutoring me, an English speaking student, in learning Chinese. Give a mini-lesson introducing the word {vocab_word}. Be concise, as there are many words to get through after. Quiz me as you go in order to move the lesson forward. When I make mistakes, you should correct and remember those mistakes. When I ask questions, you should answer in mostly English and remember those questions. Only include pinyin for new words."""
+    if user_prompt is None:
+        print("user_prompt is None")
+        user_prompt = f"Guide me through the word {vocab_word}. During the lesson (e.g., when providing example sentences using {vocab_word}), incorporate usage of {aux_words[:]}. You should provide an example sentence before asking me to provide one."
+
     messages=[
-            {"role": "system", "content": f"You are a Chinese language professor tutoring me, an English speaking student, in learning Chinese. Give a mini-lesson introducing the word {vocab_word}. Be concise, as there are many words to get through after. Quiz me as you go in order to move the lesson forward. When I make mistakes, you should correct and remember those mistakes. When I ask questions, you should answer in mostly English and remember those questions. Only include pinyin for new words."},
-            {"role": "user", "content": f"Guide me through the word {vocab_word}. During the lesson (e.g., when providing example sentences using {vocab_word}), incorporate usage of {aux_words[:]}. You should provide an example sentence before asking me to provide one."},
-            {"role": "assistant", "content": f"Let's start with the etymology of {vocab_word}..., then we'll move on to discuss some example senntences..."},
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt},
         ]
     return messages
 
